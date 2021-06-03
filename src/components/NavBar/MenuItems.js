4afa0803adf573f71ee30454208356ccdef7fa2c
francoechegoyen <72000493+francoechegoyen@users.noreply.github.com>
2021-06-03 02:06:56 -0300

    Cambios para el segundo desafío de CoderHouse

---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+   {
+       title: 'Inicio',
+       url: '#',
+       cName: 'nav-links'
+   },
+   {
+    title: 'Proteínas',
+    url: '#',
+    cName: 'nav-links'
+   },
+   {
+    title: 'Ganadores de peso',
+    url: '#',
+    cName: 'nav-links'
+   },
+   {
+    title: 'Quemadores de grasa',
+    url: '#',
+    cName: 'nav-links'
+   },
+   {
+    title: 'Contacto',
+    url: '#',
+    cName: 'nav-links'
+   },
+   {
+   title: 'Ingresar',
+   url: '#',
+   cName: 'nav-links-mobile'
+  },
+]
