13d0c30fa0445e7e9339525df76154ea2d4ce6b7
francoechegoyen <72000493+francoechegoyen@users.noreply.github.com>
2021-06-29 22:54:33 -0300

    CartContext

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Grid } from '@material-ui/core';
 import LocalShippingIcon from '@material-ui/icons/LocalShipping';
@@ -8,6 +8,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import { ItemCount } from '../ItemCount/ItemCount';
 import { itemDetailStyles } from './ItemDetailStyles';
 import { FinishPurchaseButton } from '../FinalizarCompra/FinalizarCompra';
+import { CartContext } from '../../Context/CartContext';
 
 const useStyle = makeStyles((theme) => itemDetailStyles(theme));
 
@@ -17,14 +18,17 @@ export const ItemDetail = props => {
     const { productDetail } = props;
     const [cantidadProducto, setCantidadProducto] = useState(0)
     const [click, setClick] = useState(false)
+    const {addItem, removeItem} = useContext(CartContext)
 
     const onAdd = cantidad => {
         setCantidadProducto(cantidad);
         setClick(true);
+        addItem({item: productDetail, quantity: cantidad});
     }
 
     const clickCancelar = cl =>{
         setClick(false);
+        removeItem(productDetail.id);
     }
 
     return<>
